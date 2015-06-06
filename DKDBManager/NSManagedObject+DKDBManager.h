6cc19b5145d8d2615786a28a2d1260dbd657f0e1
kevindelord <delord.kevin@gmail.com>
2015-06-06 20:23:07 +0200

    Add doc: uniqueIdentifier, saveEntityAsNotDeprecated

--- DKDBManager/NSManagedObject+DKDBManager.h
+++ DKDBManager/NSManagedObject+DKDBManager.h
@@ -24,7 +24,43 @@ typedef NS_ENUM(NSInteger, DKDBManagedObjectState) {
 
 #pragma mark - READ
 
+/**
+ * Returns the unique identifier of the current entity.
+ *
+ * When using the DKDBManager to match a distant database (behind an API)
+ * the logic to remove deprecated objects actually use a `uniqueIdentifier`
+ * for every class.
+ * The identifer of each entity is saved zhen refreshing/creating the entities.
+ * It is used when the function `removeDeprecatedEntities` is called.
+ * The default value use the native `objectID` property of a NSManagedObject.
+ *
+ * Override this method to use a custom unique identifer build from a
+ * class attribute or anything else.
+ *
+ * @return the unique identifer of the current model entity.
+ */
 - (id)uniqueIdentifier;
+
+/**
+ * Function to save/store the current object and all its child relations as not deprecated.
+ *
+ * This function is required when matching an online database behind an API.
+ * If the current class model does NOT have any child entities / relations, then 
+ * it is not required to implement it.
+ *
+ * This function is automatically called after creating an entity with: `createEntityFromDictionary:`.
+ * Unless the returned state is `.Delete` the current/new entity will be stored as not deprecated.
+ * When overriden, call `super` and forward the process to all child entities.
+ *
+ * The default behavior of the current function is to forward the saving process to its child objects.
+ * When the entity is created but its child can not be at the same time, then the developper has to create
+ * another function to manually save the objects when they have been checked/created.
+ * In few words, call this second function when the `pages` of a `book` has been created.
+ * On the second start, when the `book` did not get updated it will got saved with `saveEntityAsNotDeprecated`.
+ * The `pages` won't be saved until the second custom function is called.
+ *
+ * @return nothing
+ */
 - (void)saveEntityAsNotDeprecated;
 - (NSString *)invalidReason;
 - (BOOL)shouldUpdateEntityWithDictionary:(NSDictionary *)dictionary;
