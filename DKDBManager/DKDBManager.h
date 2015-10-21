af517e78a30d92105b50fd83bd2a096ae871e5d9
kevindelord <delord.kevin@gmail.com>
2015-10-21 13:34:02 +0200

    Import Magical Record as a framework

--- DKDBManager/DKDBManager.h
+++ DKDBManager/DKDBManager.h
@@ -10,7 +10,7 @@
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
-#import "MagicalRecord.h"
+#import <MagicalRecord/MagicalRecord.h>
 #import "NSManagedObject+DKDBManager.h"
 #import "NSManagedObject+ExistingObject.h"
 
